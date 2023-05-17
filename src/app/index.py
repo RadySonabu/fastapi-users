fb1306803b197c73daa7f2e27f77f84b70e09c19
RadySonabu <ardynubanos@gmail.com>
2023-05-17 13:43:54 +0800

    Updated path

--- src/app/index.py
+++ src/app/index.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, Request
 from mangum import Mangum
 from fastapi.responses import HTMLResponse
 from .app.app import templates
-from .api.app import api
-from .app.app import route
+# from .api.app import api
+# from .app.app import route
 
 app = FastAPI()
 
@@ -18,7 +18,7 @@ async def read_item(request: Request, id: str):
     return templates.TemplateResponse("item.html", {"request": request, "id": id})
 
 # app.mount("/api", api)
-app.mount("/ui", route)
+# app.mount("/ui", route)
 
 # For AWS deployment
 handler = Mangum(app=app)
