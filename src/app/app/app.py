50370f141ef5d7265e93cdba11e06546de93a2f0
RadySonabu <ardynubanos@gmail.com>
2023-05-17 12:37:08 +0800

    Updated paths

--- src/app/app/app.py
+++ src/app/app/app.py
@@ -1,11 +1,15 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+load_dotenv()  # take environment variables from .env.
 route = FastAPI()
-route.mount("/static", StaticFiles(directory="src/app/app/static"), name="static")
-templates = Jinja2Templates(directory="src/app/app/templates")
+route.mount("/static", StaticFiles(directory=os.getenv('static_path', 'src/app/app/static')), name="static")
+templates = Jinja2Templates(directory=os.getenv('template_path', 'src/app/app/template'))
 
 
 @route.get("/items/{id}", response_class=HTMLResponse)
