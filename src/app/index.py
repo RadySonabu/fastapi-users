c2dc92200661263afee34b43035f1aa6bb497660
RadySonabu <ardynubanos@gmail.com>
2023-05-17 13:30:18 +0800

    Updated blank lines

--- src/app/index.py
+++ src/app/index.py
@@ -12,6 +12,7 @@ app = FastAPI()
 async def index():
     return {"message": "Hello World!"}
 
+
 @route.get("sample/items/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse("item.html", {"request": request, "id": id})
